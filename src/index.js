308c8d01336a43b78330686b3c0cfde289c19823
Lucas Sena <lucassena.rj@gmail.com>
2021-05-08 03:30:01 +0100

    feat: add Results page

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import HomePage from "./pages/Home";
+import ResultsPage from "./pages/Results";
 import reportWebVitals from './reportWebVitals';
 import "./index.css";
 
@@ -9,6 +10,7 @@ ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route path="/" component={HomePage} exact={true} />
+      <Route path="/results" component={ResultsPage} />
     </Switch>
   </BrowserRouter>,
   document.getElementById("root")
---
+++ src/pages/Results/index.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import Book from "./../../components/Book";
+import Layout from "./../../components/Layout";
+import Loader from "./../../components/Loader";
+import "./style.css";
+
+function ResultsPage() {
+  const { state } = useLocation();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1500);
+  }, []);
+
+  return (
+    <Layout>
+      <section id="blog" className="blog-area mt-50 mb-50">
+        { loading ? <Loader /> : (
+            <div className="container">
+              <div className="row justify-content-center">
+                <div className="col-lg-4">
+                  <div className="section-title text-center pb-20 wow fadeInUp" data-wow-duration="1s" data-wow-delay="0.3s">
+                    <h6 className="sub-title">Search result for</h6>
+                    <h4 className="title">{ state }</h4>
+                  </div>
+                </div>
+              </div> 
+              <div className="row justify-content-center">
+                <Book />
+                <Book />
+                <Book />
+                <Book />
+                <Book />
+                <Book />
+              </div>
+            </div>
+          )
+        }
+      </section>
+    </Layout>
+  );
+}
+
+export default ResultsPage;
