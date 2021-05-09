2c9713560864a54f1abbb0ee682e8f66c1bd53b2
Lucas Sena <lucassena.rj@gmail.com>
2021-05-10 00:20:38 +0100

    fix: fIx i18n

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,13 +2,16 @@ import React, { useEffect } from "react";
 import Header from "./../Header";
 import Footer from "./../Footer";
 import WOW from "wowjs";
+import { useTranslation } from 'react-i18next';
 import "./style.css";
 
 function Layout({ children }) {
+  const { i18n } = useTranslation();
   useEffect(() => {
     new WOW.WOW({
       live: false
     }).init();
+    i18n.changeLanguage("en");
   }, []);
 
   return (
