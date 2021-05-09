0323c4c22e69bed45dc914ece49edd3ea81772b9
Lucas Sena <lucassena.rj@gmail.com>
2021-05-09 20:37:03 +0100

    feat: add i18n

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import HomePage from "./pages/Home";
 import ResultsPage from "./pages/Results";
 import reportWebVitals from './reportWebVitals';
+import "./i18n";
 import "./index.css";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Switch>
-      <Route path="/" component={HomePage} exact={true} />
-      <Route path="/results" component={ResultsPage} />
-    </Switch>
-  </BrowserRouter>,
+  <Suspense fallback="loading">
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={HomePage} exact={true} />
+        <Route path="/results" component={ResultsPage} />
+      </Switch>
+    </BrowserRouter>
+  </Suspense>,
   document.getElementById("root")
 );
 
