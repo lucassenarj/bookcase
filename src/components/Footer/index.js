76932c1e2ee2432a04e079f9ee6022a9a55e1267
Lucas Sena <lucassena.rj@gmail.com>
2021-05-10 15:52:31 +0100

    refactor: add translation footer text

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,16 +1,21 @@
 import React from "react";
 import Background from "./../../assets/images/footer-bg.jpg";
+import { useTranslation } from 'react-i18next';
 import "./style.css";
 
 function Footer(){
+  const year = new Date().getFullYear();
+  const { t } = useTranslation();
+
   return (
     <footer id="footer" className="footer-area" style={{ backgroundImage: `url(${Background})` }}>
       <div className="container">
         <div className="footer-copyright text-center">
           <p className="text">
-            © 2022
-            Developed by <a href="https://lucassenarj.github.io/" rel="nofollow">Luacs Sena</a>
-            &nbsp; layout by <a href="https://uideck.com" rel="nofollow">UIdeck</a> All Rights Reserved.
+            © {year}
+            { t("components.footer.developed_by") } <a href="https://lucassenarj.github.io/" rel="nofollow" title="Lucas Sena">Luacs Sena</a>
+            &nbsp; { t("components.footer.layout_by") } <a href="https://uideck.com" rel="nofollow" title="UIdeck">UIdeck</a>
+            { t("components.footer.all_rights_reserved") }
           </p>
         </div>
       </div>
