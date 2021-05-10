25c24115ac47a00327d1192f443b55fdccc3d10d
Lucas Sena <lucassena.rj@gmail.com>
2021-05-10 15:45:50 +0100

    refactor: traslate navitems

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,10 +2,13 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import NavItem from "./../NavItem";
 import Logo from "./../../assets/images/logo.png";
+import { useTranslation } from 'react-i18next';
 import "./style.css";
 
 function Header() {
   const [display, setDisplay] = useState(false);
+  const { t } = useTranslation();
+
   return (
     <header className="header-area">
       <div className="navbar-area headroom">
@@ -14,7 +17,7 @@ function Header() {
             <div className="col-lg-12">
               <nav className="navbar navbar-expand-lg">
                 <Link to="/">
-                  <img src={Logo} alt="Logo" />
+                  <img src={Logo} alt="Bookcase" />
                 </Link>
                 <button onClick={() => setDisplay(!display)} className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                   <span className="toggler-icon"></span>
@@ -24,11 +27,11 @@ function Header() {
 
                 <div className={`collapse navbar-collapse sub-menu-bar ${display ? "show" : ""}`} id="navbarSupportedContent">
                   <ul id="nav" className="navbar-nav m-auto">
-                    <NavItem to="/" title="Home" />
-                    <NavItem to="/search" title="Search" />
-                    <NavItem to="/favorites" title="My books" />
-                    <NavItem to="/about" title="About" />
-                    <NavItem to="/contact" title="Contact" />
+                    <NavItem to="/" title={ t("components.header.navitems.home") } />
+                    <NavItem to="/search" title={ t("components.header.navitems.search") } />
+                    <NavItem to="/favorites" title={ t("components.header.navitems.my_books") } />
+                    <NavItem to="/about" title={ t("components.header.navitems.about") } />
+                    <NavItem to="/contact" title={ t("components.header.navitems.contact") } />
                   </ul>
                 </div>
               </nav>
