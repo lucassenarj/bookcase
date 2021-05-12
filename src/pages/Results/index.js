531972da9af96e3261e21f3bb925e6b2147c3e4b
Lucas Sena <lucassena.rj@gmail.com>
2021-05-12 22:45:46 +0100

    refactor: results page

--- src/pages/Results/index.js
+++ src/pages/Results/index.js
@@ -1,46 +1,31 @@
-import React, { useEffect, useState } from "react";
-import { useTranslation } from "react-i18next";
+import React from "react";
 import { useLocation } from "react-router-dom";
-import Book from "./../../components/Book";
+import { useTranslation } from "react-i18next";
+import BookList from "./../../components/BookList";
 import Layout from "./../../components/Layout";
-import Loader from "./../../components/Loader";
+import books from "./../../utils/mocks/bookList";
 import "./style.css";
 
 function ResultsPage() {
   const { state } = useLocation();
-  const [loading, setLoading] = useState(true);
   const { t } = useTranslation();
-
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 1500);
-  }, []);
 
   return (
     <Layout>
       <section className="blog-area mt-50 mb-50">
-        { loading ? <Loader /> : (
-            <div className="container">
-              <div className="row justify-content-center">
-                <div className="col-lg-4">
-                  <div className="section-title text-center pb-20 wow fadeInUp" data-wow-duration="1s" data-wow-delay="0.3s">
-                    <h6 className="sub-title">{ t("pages.results.search_result_for") }</h6>
-                    <h4 className="title">{ state }</h4>
-                  </div>
-                </div>
-              </div> 
-              <div className="row justify-content-center">
-                <Book />
-                <Book />
-                <Book />
-                <Book />
-                <Book />
-                <Book />
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-4">
+              <div className="section-title text-center pb-20 wow fadeInUp" data-wow-duration="1s" data-wow-delay="0.3s">
+                <h6 className="sub-title">{ t("pages.results.search_result_for") }</h6>
+                <h4 className="title">{ state }</h4>
               </div>
             </div>
-          )
-        }
+          </div> 
+          <div className="row justify-content-center">
+            <BookList books={books.items} />
+          </div>
+        </div>
       </section>
     </Layout>
   );
