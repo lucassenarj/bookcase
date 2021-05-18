e1b8d7d678702776f5c87a716eebaa9dbd071d9f
Lucas Sena <lucassena.rj@gmail.com>
2021-05-18 20:07:08 +0100

    refactor(homepage): refactor homepage to interact with redux

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,34 @@
-import React from "react";
-import Layout from "./../../components/Layout";
+import React, { useEffect } from "react";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import * as Actions from "store/actions";
 import Hero from "./../../components/Hero";
+import Layout from "./../../components/Layout";
+import Loader from "./../../components/Loader";
 import "./style.css";
 
-function HomePage(){
+function HomePage({ isLoading, loading }){
+  useEffect(() => {
+    loading(false);
+  }, [loading]);
+
   return (
     <Layout>
-      <Hero />
+      { isLoading ? <Loader /> : <Hero /> }
     </Layout>
   );
 };
 
-export default HomePage;
+function mapStateToProps(state) {
+  return {
+    isLoading: state.app.loading,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    ...bindActionCreators(Actions, dispatch),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
