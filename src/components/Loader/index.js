f2e8a8f4a9225d88ffa2c534d6fc366cf9bf4011
Lucas Sena <lucassena.rj@gmail.com>
2021-05-18 20:04:31 +0100

    refactor(loader): removed integration with store

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -1,43 +1,25 @@
 import React from "react";
-import { connect } from "react-redux";
-import PropTypes from "prop-types";
 import "./style.css";
 
-function Loader({ loading }) {
+function Loader() {
   return (
-    <>
-     {
-       loading && (
-        <div className="preloader">
-          <div className="loader">
-            <div className="ytp-spinner">
-              <div className="ytp-spinner-container">
-                <div className="ytp-spinner-rotator">
-                  <div className="ytp-spinner-left">
-                    <div className="ytp-spinner-circle"></div>
-                  </div>
-                  <div className="ytp-spinner-right">
-                    <div className="ytp-spinner-circle"></div>
-                  </div>
-                </div>
-              </div>
+  <div className="preloader">
+    <div className="loader">
+      <div className="ytp-spinner">
+        <div className="ytp-spinner-container">
+          <div className="ytp-spinner-rotator">
+            <div className="ytp-spinner-left">
+              <div className="ytp-spinner-circle"></div>
+            </div>
+            <div className="ytp-spinner-right">
+              <div className="ytp-spinner-circle"></div>
             </div>
           </div>
         </div>
-       )
-     }
-    </>
-  );
+      </div>
+    </div>
+  </div>
+  )
 }
 
-Loader.propTypes = {
-  loading: PropTypes.bool.isRequired,
-}
-
-function mapStateToProps(state) {
-  return {
-    loading: state.app.loading,
-  };
-}
-
-export default connect(mapStateToProps)(Loader);
+export default Loader;
--- src/components/Loader/index.test.js
+++ src/components/Loader/index.test.js
@@ -1,36 +1,11 @@
 import React from "react";
-import configureStore from "redux-mock-store";
-import { Provider } from 'react-redux';
 import { render } from "@testing-library/react";
-import store from "store";
-import Loader from "./";
-
-const mockStore = configureStore([]);
+import Loader from "./index";
 
 describe("Loader Component", () => {
   it("renders correctly", () => {
-    const { container } = render(
-      <Provider store={store}>
-        <Loader />
-      </Provider>
-    );
+    const { container } = render(<Loader />);
 
     expect(container.firstChild).toMatchSnapshot();
-  });
-
-  it("loading not showing", () => {
-    const mockedStore = mockStore({
-      app: {
-        loading: false,
-      },
-    });
-  
-    const { container } = render(
-      <Provider store={mockedStore}>
-        <Loader />
-      </Provider>
-    );
-  
-    expect(container.firstChild).toBeNull();
   });
 });
