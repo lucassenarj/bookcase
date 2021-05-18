50a0ae2a8a141c96e517a81fffcdc712d4ce3a62
Lucas Sena <lucassena.rj@gmail.com>
2021-05-18 20:09:02 +0100

    refactor(layout): remove dots from imports

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
-import Header from "./../Header";
-import Footer from "./../Footer";
+import Header from "components/Header";
+import Footer from "components/Footer";
 import WOW from "wowjs";
 import "./style.css";
 
